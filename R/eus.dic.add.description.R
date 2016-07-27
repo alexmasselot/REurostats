9216b5bae85da55c3e82492c39f30f9e62ef2735
Alexandre Masselot <amasselot@octo.com>
2016-07-27 22:07:28 +0530

    remove useless

--- R/eus.dic.add.description.R
+++
@@ -1,7 +0,0 @@
-eus.dic.add.description = function(dataset, dic.name){
-  dic = eus.dic.load(dic.name)
-  col.name = paste(dic.name, 'description', sep='.')
-  print(col.name)
-  print(colnames(dataset$data))
-  dataset$data[[col.name]] = eus.dic.load(dic.name)[dt$data[[dic.name]]]
-}
